e115c3c050042d1d22e8074528253743a1db99f2
Jack Burgess <jackburgess124@gmail.com>
2022-10-07 10:36:28 +0100

    fix: prevent infinite comment loop when bot mentions itself

--- actions/release-management/actions/comment.js
+++ actions/release-management/actions/comment.js
@@ -23,15 +23,19 @@ function createAction (serviceLocator) {
           if (ghAction !== 'created') {
             return cb(null, false)
           }
-          // If the comment contains a trigger phrase then we are good to go
-          if (checkTriggerPhrase(comment.body)) {
-            return cb(null, true)
-          }
           var suffix = '@' + serviceLocator.authedUser.username + ' '
             , repoManager = serviceLocator.repoManager(comment.repoOwner, comment.repoName)
           // Ignore anything that isn't directly trying to communicate with the bot
           if (comment.body.indexOf(suffix) === -1) {
             return cb(null, false)
+          }
+          // Ignore anything that is the bot commenting with a comment that includes @itself
+          if (comment.author === serviceLocator.authedUser.username) {
+            return cb(null, false)
+          }
+          // If the comment contains a trigger phrase then we are good to go
+          if (checkTriggerPhrase(comment.body)) {
+            return cb(null, true)
           }
           // Otherwise, let the user know the command is unknown
           repoManager.getPull(comment.issueNumber, function (error, pr) {
